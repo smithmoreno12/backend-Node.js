03786fcbac3d20349fa502fffd20c57c17649b41
smithmoreno12 <maickcupper@gmail.com>
2026-03-28 17:38:25 -0500

    implementacion de creating categories

--- src/presentation/category/controller.ts
+++ src/presentation/category/controller.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
 import { CreateCategoryDto, CustomError } from "../../domain";
+import { CategoryService } from "../services";
 
 export class CategoryController {
   //DI
-  constructor() {}
+  constructor(private readonly categoryService: CategoryService) {}
 
   private handleError(error: unknown, res: Response) {
     if (error instanceof CustomError) {
@@ -13,11 +14,16 @@ export class CategoryController {
     return res.status(500).json({ error: "Internal Server Error" });
   }
 
-  createCategory = async (req: Request, res: Response) => {
+  createCategory = (req: Request, res: Response) => {
     const [error, createCategoryDto] = CreateCategoryDto.create(req.body);
     if (error) return res.status(400).json({ error });
-    res.json(createCategoryDto);
+    this.categoryService
+      .createCategory(createCategoryDto!, req.body.user)
+      .then((category) => res.status(201).json(category))
+      .catch((error) => this.handleError(error, res));
   };
+
+  //
   getCateggories = async (req: Request, res: Response) => {
     res.json({ message: "Obtaining categories" });
   };
--- src/presentation/category/routes.ts
+++ src/presentation/category/routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { CategoryController } from "./controller";
 import { AuthMiddleware } from "../middlewares";
+import { CategoryService } from "../services";
 
 export class CategoryRoutes {
   static get routes(): Router {
     const router = Router();
-    const controller = new CategoryController();
+    const categoryService = new CategoryService();
+    const controller = new CategoryController(categoryService);
 
     // Definir las rutas
     router.get("/", controller.getCateggories);
