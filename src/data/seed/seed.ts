de37909fe075247684f8ba5fb2fa34b58190f02d
smithmoreno12 <maickcupper@gmail.com>
2026-03-30 21:13:53 -0500

    agregar funcionalidad de seed para llenar la base de datos con datos de prueba y ajustar README.md

--- src/data/seed/seed.ts
+++ src/data/seed/seed.ts
@@ -1,5 +1,11 @@
 import { envs } from "../../config";
-import { MongoDatabase } from "../mongo/mongo-database";
+import {
+  CategoryModel,
+  MongoDatabase,
+  ProductModel,
+  UserModel,
+} from "../mongo";
+import { seedData } from "./data";
 
 (async () => {
   await MongoDatabase.connect({
@@ -10,4 +16,39 @@ import { MongoDatabase } from "../mongo/mongo-database";
   await MongoDatabase.disconnect();
 })();
 
-async function main() {}
+const randomBetween = (x: number) => Math.floor(Math.random() * x);
+
+async function main() {
+  // 0. Borrar todo!
+  await Promise.all([
+    UserModel.deleteMany(),
+    CategoryModel.deleteMany(),
+    ProductModel.deleteMany(),
+  ]);
+
+  //1 Crear usuarios
+  const users = await UserModel.insertMany(seedData.users);
+
+  //2 Crear categorias
+  const categories = await CategoryModel.insertMany(
+    seedData.categories.map((c) => {
+      return {
+        ...c,
+        user: users[0].id,
+      };
+    }),
+  );
+
+  //3 Crear productos
+  const products = await ProductModel.insertMany(
+    seedData.products.map((p) => {
+      return {
+        ...p,
+        user: users[randomBetween(seedData.users.length - 1)].id,
+        category: categories[randomBetween(categories.length - 1)].id,
+      };
+    }),
+  );
+
+  console.log("SEED");
+}
